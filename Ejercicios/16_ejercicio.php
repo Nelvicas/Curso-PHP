2da1b1483086328f2a3f3541fd4f05ca7ef4bddc
nelvi <nelvicas97@gmail.com>
2026-05-12 13:36:51 -0600

    funciones

---
+++ Ejercicios/16_ejercicio.php
@@ -0,0 +1,18 @@
+<!--  Funciones  -->
+
+
+<?php
+
+function imprimirNombre($nombre, $apellido){
+
+    // Rutinas (instrucciones)
+    echo "Hola ".$nombre." ".$apellido."<br>";
+
+}
+
+// Llamar ala funcion
+imprimirNombre("Oscar", "uh");
+imprimirNombre("Pedro", "Perez");
+imprimirNombre("Maria", "Lopez");
+
+?>
