e1d7fe27743156e8fbdb2389b9cd2936ada9a46b
nelvi <nelvicas97@gmail.com>
2026-05-13 15:01:34 -0600

    funciones para arreglos en php (array_push)

---
+++ Ejercicios/23_ejercicio.php
@@ -0,0 +1,12 @@
+<!--  Funciones para arreglos en php  -->
+
+<?php
+
+$frutas= array("Fresa", "Manzana", "Pera");
+
+array_push($frutas, "Uva");
+array_push($frutas, "Naranja");
+
+print_r($frutas);
+
+?>
