c0480f280965fc0ca7fe39c81564cdfaaef245da
nelvi <nelvicas97@gmail.com>
2026-05-12 21:20:41 -0600

    funciones matematicas, funciones propias de php

---
+++ Ejercicios/17_ejercicio.php
@@ -0,0 +1,12 @@
+<!-- Funciones matematicas   -->
+
+
+<?php
+
+// numero aleatorio
+$numeroAleatorio = rand(1,10);
+
+echo $numeroAleatorio;
+
+
+?>
---
+++ Ejercicios/18_ejercicio.php
@@ -0,0 +1,12 @@
+<!-- Funciones propias de php -->
+
+
+<?php
+
+$nombre = "Oscar Uh";
+
+$nombreMayusculas= strtoupper($nombre);
+
+echo $nombreMayusculas;
+
+?>
