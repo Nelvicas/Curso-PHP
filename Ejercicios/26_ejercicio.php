954925d2c553e08f1d68906dafe8f08c22a96359
nelvi <nelvicas97@gmail.com>
2026-05-16 23:29:20 -0600

    herencia

---
+++ Ejercicios/26_ejercicio.php
@@ -0,0 +1,44 @@
+<!--  Herencia
+
+-->
+
+<?php
+
+class persona{
+
+    public $nombre;      // propiedades
+    private $edad;
+    protected $altura;
+
+
+    public function asignarNombre($nuevoNombre){       // acciones o metodos
+        $this->nombre=$nuevoNombre;
+    }
+
+    public function imprimirNombre(){
+        echo "Hola soy ".$this->nombre;
+    }
+
+    public function mostrarEdad(){
+        $this->edad = 20;
+        return $this->edad; 
+    }
+}
+
+class trabajador extends persona{
+
+    public $puesto;    // nueva propiedad
+
+    public function presentarseComoTrabajador(){
+        echo "Hola soy ".$this->nombre." y soy un ".$this->puesto;
+    }
+}
+
+
+$objTrabajador= new trabajador();       // instancia o creacion de un objeto
+$objTrabajador->asignarNombre("Oscar Perez");      // llamando un metodo
+$objTrabajador->puesto ="Profesor";
+
+$objTrabajador->presentarseComoTrabajador();
+
+?>
