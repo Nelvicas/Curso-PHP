eb7e048e15eb7ac8d96035104f649a4e4e7e7f3a
nelvi <nelvicas97@gmail.com>
2026-05-24 23:18:26 -0600

    valores del input

---
+++ Ejercicios/34_ejercicios.php
@@ -0,0 +1,36 @@
+<!--  Valores del input radio   --->
+
+
+
+<?php
+
+    $txtNombre="";
+    if($_POST){
+        $txtNombre= (isset($_POST['txtNombre']))?$_POST['txtNombre']:"";   // if ternario 
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+
+    <?php if($_POST){ ?>
+    <strong> Hola </strong>: <?php echo $txtNombre;?>
+    <?php } ?>
+
+    <form action="33_ejercicio.php" method="post">
+
+        <input value="<?php echo $txtNombre;?>" type="text" name="txtNombre" id="">
+
+        <input type="submit" value="Enviar Informacion">
+
+    </form>
+    
+</body>
+</html>
