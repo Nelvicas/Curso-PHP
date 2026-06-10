66f30e5a02abe260e561157bf77c51b0db5324b7
nelvi <nelvicas97@gmail.com>
2026-06-10 07:36:26 -0600

    integracion de booststrap

--- Portafolio_web/index.php
+++ Portafolio_web/index.php
@@ -1,5 +1,14 @@
 <?php  include('cabecera.php');  ?>
 
-    Hola develoteca soy tu index!!
+<div class="p-5 bg-light">
+    <div class="container">
+        <h1 class="display-3">Bienvenid@s</h1>
+        <p class="lead"> Este es un portafolio privado </p>
+        <hr class="my-2">
+        <p>Mas informacion</p>
+
+    </div>
+
+</div>
 
 <?php  include('pie.php');  ?>
