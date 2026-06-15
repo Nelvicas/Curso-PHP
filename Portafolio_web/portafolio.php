fddec866ab7cfc02336137a7c4d04f426f24f415
nelvi <nelvicas97@gmail.com>
2026-06-14 21:42:04 -0600

    insertar imagen en carpeta correspondiente

--- Portafolio_web/portafolio.php
+++ Portafolio_web/portafolio.php
@@ -9,22 +9,31 @@ if($_POST){
     print_r($_POST);
 
     $nombre= $_POST['nombre'];  // insertar datos
+    $descripcion=$_POST['descripcion'];
 
-$objConexion = new conexion();
-$sql="INSERT INTO `proyectos` (`id`, `nombre`, `imagen`, `descripcion`) VALUES (NULL, '$nombre', 'imagen.jpg', 'Es un proyecto de hace mucho tiempo');";
-$objConexion->ejecutar($sql);
+    $fecha= new DateTime();
+    $imagen=$fecha->getTimestamp()."_".$_FILES['archivo']['name'];
+
+    $imagen_temporal=$_FILES['archivo']['tmp_name'];
+    move_uploaded_file($imagen_temporal,"imagenes/".$imagen);
+
+    $objConexion = new conexion();
+    $sql="INSERT INTO `proyectos` (`id`, `nombre`, `imagen`, `descripcion`) VALUES (NULL, '$nombre', '$imagen', '$descripcion');";
+    $objConexion->ejecutar($sql);
+
+
 
 }
 
 if($_GET){
 
-    $objConexion = new conexion();
+    $objConexion = new conexion();    // ontener datos con get y eliminarlos 
     $sql= "DELETE FROM proyectos WHERE `proyectos`.`id` = ".$_GET['borrar'];
     $objConexion->ejecutar($sql);
 }
 
 
-$objConexion= new conexion();
+$objConexion= new conexion();    // consultar datos 
 $proyectos = $objConexion->consultar("SELECT * FROM `proyectos`");
 
 //print_r($proyectos);
@@ -36,48 +45,61 @@ $proyectos = $objConexion->consultar("SELECT * FROM `proyectos`");
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                 <form action="portafolio.php" method="post"  enctype="multipart/form-data">
-                    Nombre del proyecto: <input class="form-control" type="text" name="nombre" id="">
-                    <br>
-                    Imagen del proyecto: <input class="form-control" type="text" name="archivo" id="">
-                    <br>
-                    <input class="btn btn-success" type="submit" value="Enviar proyecto">
-                    
-                </form>
-               
-            </div>
-            <div class="col-md-6">
-                 <br><br>
-                    <div
-                        class="table-responsive"
-                    >
-                        <table
-                            class="table table-primary"
-                        >
-                            <thead>
-                                <tr>
-                                    <th scope="col">ID</th>
-                                    <th scope="col">Nombre</th>
-                                    <th scope="col">Imagen</th>
-                                    <th scope="col">Descripción</th>
-                                    <th scope="col">Acciones</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach($proyectos as $proyecto){ ?>
-                                    <tr class="">
-                                        <td> <?php echo $proyecto['id']; ?> </td>
-                                        <td> <?php echo $proyecto['nombre']; ?></td>
-                                        <td> <?php echo $proyecto['imagen']; ?></td>
-                                        <td> <?php echo $proyecto['descripcion']; ?></td>
-                                        <td><a name="" id="" class="btn btn-danger" href="?borrar=<?php echo $proyecto['id']; ?>" role="button" >Eliminar</a> </td>
-                                    </tr>
-                                <?php } ?>
-                                
-                            </tbody>
-                        </table>
+
+                <div class="card">
+                    <div class="card-header">
+                        Datos del proyecto
                     </div>
-            </div>
+                    <div class="card-body">
+
+                 
+                            <form action="portafolio.php" method="post"  enctype="multipart/form-data">
+                                    Nombre del proyecto: <input class="form-control" type="text" name="nombre" id="">
+                                    <br>
+                                    Imagen del proyecto: <input class="form-control" type="file" name="archivo" id="">
+                                    <br>
+                                    Descripción:
+                                    <textarea class="form-control" name="descripcion" id="" row="3"></textarea>
+                                    <br>
+                                    <input class="btn btn-success" type="submit" value="Enviar proyecto">
+                                    
+                            </form>
+                            
+                            </div>
+                    </div>
+                </div>
+                            <div class="col-md-6">
+                                <br>
+                                    <div
+                                        class="table-responsive"
+                                    >
+                                        <table
+                                            class="table table-primary"
+                                        >
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">ID</th>
+                                                    <th scope="col">Nombre</th>
+                                                    <th scope="col">Imagen</th>
+                                                    <th scope="col">Descripción</th>
+                                                    <th scope="col">Acciones</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php foreach($proyectos as $proyecto){ ?>
+                                                    <tr class="">
+                                                        <td> <?php echo $proyecto['id']; ?> </td>
+                                                        <td> <?php echo $proyecto['nombre']; ?></td>
+                                                        <td> <?php echo $proyecto['imagen']; ?></td>
+                                                        <td> <?php echo $proyecto['descripcion']; ?></td>
+                                                        <td><a name="" id="" class="btn btn-danger" href="?borrar=<?php echo $proyecto['id']; ?>" role="button" >Eliminar</a> </td>
+                                                    </tr>
+                                                <?php } ?>
+                                                
+                                            </tbody>
+                                        </table>
+                                    </div>
+                            </div>
             
         </div>
     </div>
