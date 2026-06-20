de411f0f871730ecce9b3f92f50a1347f44d5bd6
nelvi <nelvicas97@gmail.com>
2026-06-20 17:20:40 -0600

    estructura de carpetas, inportar booststrap

---
+++ Sitio_Web/index.php
@@ -0,0 +1,30 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio Web</title>
+
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+</head>
+<body>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <ul class="nav navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#" aria-current="page"
+                        >Nav 1 <span class="visually-hidden">(current)</span></a
+                    >
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nav 2</a>
+                </li>
+            </ul>
+        </nav>
+        
+    
+</body>
+</html>
